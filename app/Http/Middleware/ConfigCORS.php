b7fa21f62b4c36d3420aef5ea187f71a726b5771
catheali <aliciacatherinealencar@gmail.com>
2023-05-22 16:24:02 -0300

    Excluindo user sem validação token

--- app/Http/Middleware/ConfigCORS.php
+++ app/Http/Middleware/ConfigCORS.php
@@ -16,23 +16,31 @@ class ConfigCORS
      */
     public function handle($request, Closure $next)
     {
-        header("Access-Control-Allow-Origin: *");
-
-        $headers = [
-            'Access-Control-Allow-Methods'      => 'POST, GET, OPTIONS, PUT, DELETE',
-            'Access-Control-Allow-Headers'      => 'Content-Type, X-Auth-Token, Origin, Authorization',
-            'Access-Control-Allow-Credentials'  => true,
-            'Cache-Control'                     => 'no-store, no-cache, must-revalidate',
-        ];
-        if ($request->getMethod() == "OPTIONS") {
-            return response('OK')
-                ->withHeaders($headers);
-        }
-
         $response = $next($request);
-        foreach ($headers as $key => $value)
-            $response->header($key, $value);
+
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
         return $response;
+
+        // header("Access-Control-Allow-Origin: *");
+
+        // $headers = [
+        //     'Access-Control-Allow-Methods'      => 'POST, GET, OPTIONS, PUT, DELETE',
+        //     'Access-Control-Allow-Headers'      => 'Content-Type, X-Auth-Token, Origin, Authorization',
+        //     'Access-Control-Allow-Credentials'  => true,
+        //     'Cache-Control'                     => 'no-store, no-cache, must-revalidate',
+        // ];
+        // if ($request->getMethod() == "OPTIONS") {
+        //     return response('OK')
+        //         ->withHeaders($headers);
+        // }
+
+        // $response = $next($request);
+        // foreach ($headers as $key => $value)
+        //     $response->header($key, $value);
+        // return $response;
 
         // return $next($request)
         //     ->header('Access-Control-Allow-Origin', '*')
